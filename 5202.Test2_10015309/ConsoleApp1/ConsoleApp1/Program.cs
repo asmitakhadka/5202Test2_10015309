74845e53982d215de1982a23508ddcbf5ea7ba20
Asmita Khadka <10015309@student.toiohomai.ac.nz>
2018-09-27 14:57:06 +1200

    test

---
+++ 5202.Test2_10015309/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            askbirdname();
+            askbirdspecies();
+            askbirdsex();
+            askbirdweight();
+            askbirdbandno();
+            askbirdoutput();
+            askbird();
+        }
+        
+        public static void askbirdname()
+        {
+           
+
+            Console.WriteLine("Please enter the bird name ");
+            Console.ReadLine();
+        } 
+        public static void askbirdspecies()
+        {
+            Console.WriteLine("Please enter the bird species ");
+            Console.ReadLine();
+        }
+        public static void askbirdsex()
+        {
+            Console.WriteLine("Please enter the bird sex ");
+            Console.ReadLine();
+        }
+        public static void askbirdweight()
+        {
+           Console.WriteLine("Please enter the bird weight ");
+           Console.ReadLine();
+        }
+        public static void askbirdbandno()
+        { 
+            Console.WriteLine("Please enter the bird bandno ");
+            Console.ReadLine();
+        }
+        public static void askbird()
+        {
+            Console.Clear();
+            Console.WriteLine("Information about the New Zealand Birds of Prey that have rescused and banded");
+            Console.WriteLine("Bird object created and default data loaded...");
+
+            Console.WriteLine("Please Update Bird Data");
+          
+        }
+        public static void askbirdoutput()
+        {
+            Console.WriteLine("BIRD OF PERY DATA OUTPUT");
+            Console.WriteLine("Bird Name: Hisan");
+            Console.WriteLine("Bird Species: Nz Falcon");
+            Console.WriteLine("Bird Sex: Male");
+            Console.WriteLine("Bird Weight: 240grams");
+            Console.WriteLine("Bird Bandno: H39878");
+        }
+        
+    }
+}
